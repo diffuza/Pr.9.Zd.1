b6bbd3482d32b180463f81c810e770502d6302b2
USER <USER@DESKTOP-TRQG01D>
2022-10-18 15:27:55 +0300

    Добавьте файлы проекта.

---
+++ Pr.9.Zd.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+                Console.WriteLine(i + " " + Convert.ToDouble(i * 0.453));
+        }
+    }
+}
